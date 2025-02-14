e64ab8ff6f1deb0fb1984cf02ff0a13c471ed23a
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-02-14 11:46:07 +0300

    Reset

--- server.js
+++ server.js
@@ -4,16 +4,11 @@ require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const cors = require("cors");
 
-const authRoutes = require("./routes/authRoutes");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-
-// Routes
-app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to The Online Hair Specialist Finder Backend!");
