197f1a0c1ee258c03e4dd8f292edb6c67e34a9f8
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-02-26 09:38:23 +0300

    Supabasw

--- server.js
+++ server.js
@@ -408,7 +408,7 @@ app.post("/api/appointments", async (req, res) => {
   // Get appointment details
 app.get("/api/appointments/:id", async (req, res) => {
   const { id } = req.params;
-  const appointment = await pool.query("SELECT * FROM appointments WHERE id = $1", [id]);
+  const appointment = await supabase.query("SELECT * FROM appointments WHERE id = $1", [id]);
   res.json(appointment.rows[0]);
 });
 
