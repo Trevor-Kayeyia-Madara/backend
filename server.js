d8b6ba17c572d2e9ca738a28a6bdb94f0c4145f9
Theeicon45 <judahkinyanjui@gmail.com>
2025-03-22 00:41:27 +0300

    .

--- server.js
+++ server.js
@@ -789,6 +789,7 @@ app.get("/api/specialist/:userId", async (req, res) => {
 
 
 
+
 // ✅ Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
