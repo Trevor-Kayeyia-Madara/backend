365c1a1418611b6f643c304d849a4d1ddcdd0de2
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-04-13 22:18:15 +0300

    Set it

--- server.js
+++ server.js
@@ -1042,15 +1042,6 @@ app.post("/api/chats/create", async (req, res) => {
     }
 });
 
-
-
-
-
-
-
-
-
-
 // ✅ Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
